1eb749b7914f966aba34097f6588f750979eeeb2
Vítor Cruz <vittorcruz18101998@gmail.com>
2025-02-07 21:39:32 -0300

    feat: post user

---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,22 @@
+package com.ms.user.services;
+
+import org.springframework.stereotype.Service;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserService {
+  private final UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Transactional
+  public UserModel save(UserModel userModel) {
+    return this.userRepository.save(userModel);
+  }
+}
